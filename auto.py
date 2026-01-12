ec3c4c54516b2b308905a5eb99e2c392ad651238
Kais-Boersma <kais.boersma@student.uva.nl>
2026-01-12 15:12:02 +0100

    Added 2 points to the 3d model
    Added 2 additiontal points using min/max.

--- auto.py
+++ auto.py
@@ -1,7 +1,7 @@
 import numpy as np
 import open3d as o3d
 
-PLY_FILE = "test1.ply"  # change if needed
+PLY_FILE = "test.ply"  # change if needed
 
 
 # -------------------------------
@@ -89,21 +89,68 @@ print("Lowest point (Y):", lowest_point)
 # -------------------------------
 # Rotate bone around Y to face lowest point toward viewer (+Z)
 # -------------------------------
-# Vector from origin to lowest point in XZ plane
 vec = lowest_point[[0, 2]]  # x and z
-angle = np.arctan2(vec[0], vec[1])  # angle around Y to rotate
-
-# Rotate around Y (up) by -angle
+angle = np.arctan2(vec[0], vec[1])  # rotation around Y
 R_y = geometry.get_rotation_matrix_from_axis_angle(np.array([0, -angle, 0]))
 geometry.rotate(R_y, center=(0, 0, 0))
 
+# Re-fetch points after rotation
+if isinstance(geometry, o3d.geometry.TriangleMesh):
+    points = np.asarray(geometry.vertices)
+else:
+    points = np.asarray(geometry.points)
+
 # -------------------------------
-# Create big point (sphere)
+# Find "most left" point in bottom 15% of bone
 # -------------------------------
-sphere = o3d.geometry.TriangleMesh.create_sphere(radius=4.0)
-sphere.paint_uniform_color([1.0, 0.0, 0.0])  # red
-sphere.translate(lowest_point)
-sphere.compute_vertex_normals()
+Y_min = np.min(points[:, 1])
+Y_max = np.max(points[:, 1])
+bottom_threshold = Y_min + 0.15 * (Y_max - Y_min)
+
+bottom_points = points[points[:, 1] <= bottom_threshold]
+
+left_index = np.argmin(bottom_points[:, 0])
+left_point = bottom_points[left_index]
+print("Most left point (bottom 15%):", left_point)
+
+# -------------------------------
+# Find highest point along Y-axis line (x≈0, z≈0)
+# -------------------------------
+axis_tolerance = 1.0  # radius around Y-axis to consider "on axis"
+
+# Distance from Y-axis
+dist_from_axis = np.sqrt(points[:, 0] ** 2 + points[:, 2] ** 2)
+axis_points = points[dist_from_axis <= axis_tolerance]
+
+if len(axis_points) > 0:
+    axis_top_index = np.argmax(axis_points[:, 1])
+    axis_top_point = axis_points[axis_top_index]
+    print("Top point on Y-axis:", axis_top_point)
+else:
+    axis_top_point = None
+    print("No points found along Y-axis within tolerance!")
+
+# -------------------------------
+# Create spheres for landmarks
+# -------------------------------
+# Red = lowest
+sphere_low = o3d.geometry.TriangleMesh.create_sphere(radius=2.0)
+sphere_low.paint_uniform_color([1.0, 0.0, 0.0])
+sphere_low.translate(lowest_point)
+sphere_low.compute_vertex_normals()
+
+# Blue = most left (bottom 15%)
+sphere_left = o3d.geometry.TriangleMesh.create_sphere(radius=2.0)
+sphere_left.paint_uniform_color([0.0, 0.0, 1.0])
+sphere_left.translate(left_point)
+sphere_left.compute_vertex_normals()
+
+# Green = top along Y-axis
+if axis_top_point is not None:
+    sphere_top = o3d.geometry.TriangleMesh.create_sphere(radius=2.0)
+    sphere_top.paint_uniform_color([0.0, 1.0, 0.0])
+    sphere_top.translate(axis_top_point)
+    sphere_top.compute_vertex_normals()
 
 # -------------------------------
 # Visualization
@@ -112,7 +159,10 @@ vis = o3d.visualization.Visualizer()
 vis.create_window(window_name="3D Bone Viewer", width=1200, height=900)
 
 vis.add_geometry(geometry)
-vis.add_geometry(sphere)
+vis.add_geometry(sphere_low)
+vis.add_geometry(sphere_left)
+if axis_top_point is not None:
+    vis.add_geometry(sphere_top)
 
 opt = vis.get_render_option()
 opt.background_color = np.array([0.05, 0.05, 0.05])
